c0a897def6c18d167ee3e467e282ce97a4ce156e
Priyam Upadhyay <priyamupadhyay17@msitprogram.net>
2022-03-22 00:59:51 +0530

    updated few things

--- battleship.py
+++ battleship.py
@@ -186,10 +186,6 @@ Parameters: 2D list of ints
 Returns: bool
 '''
 def isVertical(ship):
-    '''ship.sort()
-    if (ship[0][1] == ship[1][1] == ship[2][1]) and (ship[0][0] + 1 == ship[1][0] == ship[2][0] - 1):
-        return True
-    return False'''
     (x,y)=ship[0]
     count = 0
     lst = []
@@ -216,10 +212,6 @@ Parameters: 2D list of ints
 Returns: bool
 '''
 def isHorizontal(ship):
-    '''ship.sort()
-    if (ship[0][0] == ship[1][0] == ship[2][0]) and (ship[0][1] + 1 == ship[1][1] == ship[2][1] - 1):
-        return True
-    return False'''
     (x,y)=ship[0]
     count = 0
     lst = []
